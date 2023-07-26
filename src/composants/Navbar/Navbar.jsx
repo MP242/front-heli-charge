75d9fb62612355547d9dd5076532a676decd4dbc
MP242 <marc.pollet@epitech.eu>
2023-07-27 02:18:09 +0400

    fix beug logout

--- src/composants/Navbar/Navbar.jsx
+++ src/composants/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 // import hamburger from '../../assets/hamburger.svg';
 import './Navbar.scss';
@@ -7,11 +7,13 @@ import HamburgerMenu from '../Hamburger/HamburgerMenu';
 
 export const Navbar = ({isLoggedIn, setIsLoggedIn})=> {
   let user = localStorage.getItem('user');
+  const navigate = useNavigate();
   const handleLogout = () => {
     user = localStorage.removeItem('user');
     // localStorage.removeItem('token');
     // localStorage.removeItem('userName');
     setIsLoggedIn(false);
+    navigate('/');
   };
 
   return (
