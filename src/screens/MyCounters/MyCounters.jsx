815f9b94eeecae925416b1ff8c22026fce084e08
MP242 <marc.pollet@epitech.eu>
2023-07-27 02:06:32 +0400

    fix beug

--- src/screens/MyCounters/MyCounters.jsx
+++ src/screens/MyCounters/MyCounters.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './MyCounters.scss';
-import { useGetSessionCounter } from '../../hooks/sessionCounter/useGetSessionCounter';
 import { Accordion } from '../../composants/Accordion/Accordion';
 
 export const MyCounters = () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  const { loading, AllSessionCounter, getAllSessionCounter } =
-    useGetSessionCounter();
+  const [AllSessionCounter, setAllSessionCounter] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    getAllSessionCounter(user.userId);
-  }, [user.userId]);
+    console.log("useEffect !!!")
+    const user = JSON.parse(localStorage.getItem('user'));
+    console.log("user.userID", user.userId)
+    setLoading(true);
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://server-heli-charge-706f4d31d3fe.herokuapp.com/counters/user/${user.userId}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Get all Session Counter message:', data);
+        setAllSessionCounter(data);
+        setLoading(false);
+      })
+      .catch((error) => console.log('error', error));
+  }, [user.userID]);
 
   return (
-    <div className='MyCounters'>
+    <div className="MyCounters">
       {loading && <p>Loading...</p>}
       {AllSessionCounter.map((sessionCounter) => {
         const date = new Date(sessionCounter.created_at);
